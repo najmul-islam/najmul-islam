ffcbeae817ed8e2a73b97da34ebbecb433ad2068
najmul-islam <najmulislam519@gmail.com>
2024-10-19 11:10:34 +0600

    remove meta from home page

--- src/app/page.js
+++ src/app/page.js
@@ -5,17 +5,6 @@ import About from "@/components/about/About";
 import Service from "@/components/service/Service";
 import Contact from "@/components/contact/Contact";
 
-export const metadata = {
-  title: "Najmul Islam | Home",
-  description:
-    "Welcome to Najmul Islam's personal website, showcasing modern web development projects and services.",
-  keywords: ["web development", "portfolio", "reactjs", "nextjs"],
-  robots: { index: true, follow: true },
-  authors: [{ name: "Najmul Islam" }],
-  publisher: "Najmul Islam",
-  alternates: { canonical: "https://najmulislam.vercel.app" },
-};
-
 const HomePage = () => {
   return (
     <>
