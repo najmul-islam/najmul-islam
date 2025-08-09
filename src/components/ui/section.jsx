d8f77976742a665307e268f1c9b1e88bfbf88cd8
najmul-islam <najmulislam519@gmail.com>
2025-08-10 00:49:50 +0600

    delete all things from this repo

--- src/components/ui/section.jsx
+++
@@ -1,8 +0,0 @@
-const Section = ({ children, id }) => {
-  return (
-    <section className="py-10 md:py-20" id={id}>
-      <div className="container">{children}</div>
-    </section>
-  );
-};
-export default Section;
